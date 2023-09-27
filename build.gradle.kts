fc1696a58274b35d8d68e12072a221216f622aa8
David Senoner <seda18@rolmail.net>
2023-09-27 18:34:24 +0200

    Updated gradle, removed kotlin as not needed plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,7 @@ import dev.architectury.pack200.java.Pack200Adapter
 
 plugins {
     java
-    kotlin("jvm") version ("1.8.22")
-    id("gg.essential.loom") version ("1.2.8")
+    id("gg.essential.loom") version ("1.3.12")
     id("dev.architectury.architectury-pack200") version ("0.1.3")
 }
 
