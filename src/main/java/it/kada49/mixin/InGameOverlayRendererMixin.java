0597cbe97ad5563d78356dc164879815b77ee020
David Senoner <seda18@rolmail.net>
2025-06-03 17:10:54 +0200

    port to fabric 1.21.5

--- src/main/java/it/kada49/mixin/InGameOverlayRendererMixin.java
+++ src/main/java/it/kada49/mixin/InGameOverlayRendererMixin.java
@@ -3,6 +3,7 @@ package it.kada49.mixin;
 import it.kada49.DisableBurningAnimation;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameOverlayRenderer;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameOverlayRenderer.class)
 public class InGameOverlayRendererMixin {
 	@Inject(at = @At("HEAD"), method = "renderFireOverlay", cancellable = true)
-	private static void init(MinecraftClient client, MatrixStack matrices, CallbackInfo info) {
+	private static void init(MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo info) {
 		if (!DisableBurningAnimation.BURNING_ENABLED) info.cancel();
 	}
 }
